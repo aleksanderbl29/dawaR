c88bb1c3ac87847640a470b2a26b0147286217bd
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-08-17 19:02:57 +0200

    Basic version of api function is functional

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,15 @@
-dawa <- function(section, append_to_url = "") {
+dawa <- function(section,
+                 append_to_url = "",
+                 extra_params = "",
+                 verbose = TRUE,
+                 cache = TRUE) {
 
   base_url <- "https://api.dataforsyningen.dk"
 
-  if (section %in% c(
-    "adgangsadresser",
-    "adresser"
-  )) {
-    append_to_url <- "/afstemningsomraader"
-  } else if (section == "sogne") {
-    append_to_url <- "/sogne"
+  if (section %in% available_sections()) {
+    if (verbose == TRUE) {
+      cli::cli_alert("Requesting {.var {section}} from DAWA")
+    }
   } else {
     cli::cli_abort(c(
           "You have provided {.var {section}} as a section for this api.",
@@ -16,11 +17,21 @@ dawa <- function(section, append_to_url = "") {
       ))
   }
 
-  cli::cli_alert("Requesting {.var {section}} from DAWA")
-
-  httr2::request(base_url) |>
+  base_request <- httr2::request(base_url) |>
     httr2::req_url_path_append(section) |>
-    httr2::req_user_agent("dawaR (http://dawa-pkg.aleksanderbl.dk)") |>
-    httr2::req_perform() |>
+    httr2::req_url_path_append(append_to_url) |>
+    httr2::req_url_path_append(extra_params) |>
+    httr2::req_user_agent("dawaR (http://dawa-pkg.aleksanderbl.dk)")
+
+  if (cache == TRUE) {
+    temp_dir <- tempdir()     # Location for caching the response
+    dawa_request <- base_request |>
+      httr2::req_cache(temp_dir)
+  } else if (cache == FALSE) {
+    dawa_request <- base_request
+  }
+
+  httr2::req_perform(dawa_request) |>
     httr2::resp_body_json()
 }
+
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,14 +1,16 @@
-# test_that("API base function properly", {
-#   # Call unsupported part of api and return error
-#   expect_error(dawa(section = "base"))
-#
-#   old <- httr2::request(
-#     "https://api.dataforsyningen.dk/afstemningsomraader"
-#     ) |>
-#     httr2::req_perform() |>
-#     httr2::resp_body_json()
-#
-#   new <- dawa("afstemningsomraader")
-#
-#   expect_equal(old[[1]], new[[1]])
-# })
+test_that("API base function properly", {
+  # Call unsupported part of api and return error
+  expect_error(dawa(section = "base"))
+  expect_error(dawa(section = "supermarked"))
+
+
+  old <- httr2::request(
+    "https://api.dataforsyningen.dk/sogne"
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_json()
+
+  new <- dawa("sogne", verbose = FALSE)
+
+  expect_equal(old[[1]], new[[1]])
+})
