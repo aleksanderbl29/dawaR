587f9af74e47cf3b4bae44262baf5d0c64d0e5bf
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-08-23 19:16:09 +0200

    Add get map data function documentation and export function

--- R/get_map_data.R
+++ R/get_map_data.R
@@ -1,7 +1,20 @@
+#' Fetch map data to use with `{sf}`.
+#'
+#' @param type Defines the type of map data to request from DAWA. Run
+#'   `available_sections(format = "geojson")` to your options.
+#'
+#' @return Returns a `data.frame` object that contains polygons for the section
+#'   provided.
+#' @export
+#'
+#' @examples
+#' x <- get_map_data("regioner")
+#' ggplot2::ggplot(x) +
+#'   ggplot2::geom_sf()
 get_map_data <- function(type) {
 
-  if (type == "afstemningsomraader") {
-    section <- "afstemningsomraader"
+  if (!type %in% available_sections(format = "geojson", verbose = FALSE)) {
+    cli::cli_abort("You have provided type {.var {type}} that is not compatible with this function.")
   }
 
   api_response <- dawa(
@@ -14,7 +27,6 @@ get_map_data <- function(type) {
     api_response,
     quiet = TRUE
   )
-
   map_data <- sf::st_as_sf(resp_st)
 
   return(map_data)
