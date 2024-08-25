d1a65c1fc1a376180bd252d3fc76e67e93cbe750
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-08-25 18:19:56 +0200

    Run separate test for CI and RStudio

--- tests/testthat/test-get_map_data.R
+++ tests/testthat/test-get_map_data.R
@@ -1,6 +1,24 @@
-test_that("get_map_data() fetches data properly and provides in the proper format", {
-  expect_snapshot(get_map_data(type = "afstemningsomraader"))
-  expect_snapshot(get_map_data(type = "kommuner"))
-  expect_snapshot(get_map_data(type = "politikredse"))
-  expect_snapshot(get_map_data(type = "regioner"))
+test_that("get_map_data() fetches data properly and provides in the proper format when run in CI", {
+
+  is_rstudio <- .Platform$GUI == "RStudio"
+
+  if (!is_rstudio) {
+    expect_snapshot(get_map_data(type = "afstemningsomraader"))
+    expect_snapshot(get_map_data(type = "kommuner"))
+    expect_snapshot(get_map_data(type = "politikredse"))
+    expect_snapshot(get_map_data(type = "regioner"))
+  }
+
+})
+
+test_that("get_map_data() fetches data properly and provides in the proper format when run in RStudio", {
+
+  is_rstudio <- .Platform$GUI == "RStudio"
+
+  if (is_rstudio) {
+    expect_snapshot(get_map_data(type = "afstemningsomraader"))
+    expect_snapshot(get_map_data(type = "kommuner"))
+    expect_snapshot(get_map_data(type = "politikredse"))
+    expect_snapshot(get_map_data(type = "regioner"))
+  }
 })
