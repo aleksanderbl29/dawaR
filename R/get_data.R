c0ad35afc8ed8c10a107815e2198b6542695c268
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-08-30 23:20:17 +0200

    Add basic get data function to wrap for specific section data

---
+++ R/get_data.R
@@ -0,0 +1,30 @@
+get_data <- function(section,
+                     as_list = FALSE) {
+
+  if (!section %in% available_sections()) {
+    cli::cli_abort("You have provided section {.var {section}}. It is not available through the api")
+  }
+
+  response <- dawa(section = section,
+                   verbose = FALSE,
+                   struktur = "flad")
+
+  if (as_list == TRUE) {
+
+    output <- lapply(response, function(x) {
+      x[1] <- as.numeric(x[1])  # Convert the first element to double
+      x[2] <- as.numeric(x[2])  # Convert the second element to double
+      return(x)
+    })
+
+  } else if (as_list == FALSE) {
+    df <- do.call(rbind.data.frame, response)
+    df[[1]] <- as.double(df[[1]])
+    df[[2]] <- as.double(df[[2]])
+
+    output <- df
+  }
+
+  return(output)
+
+}
