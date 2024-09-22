038030e5bc23dea5ea4db1d2d720529d3ba67a67
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-22 15:36:50 +0200

    Further testing of map function alternatives

--- benchmarking/map_data.R
+++ benchmarking/map_data.R
@@ -1,11 +1,64 @@
 manual_passthrough <- function() {
-  dawaR::dawa("regioner", format = "geojson", verbose = FALSE) |>
+  dawaR::dawa("sogne", format = "geojson", verbose = FALSE) |>
     sf::read_sf() |>
     sf::st_as_sf()
 }
 
-microbenchmark::microbenchmark(
-  dawaR::get_map_data("regioner"),
+download_file <- function() {
+  temp_file <- tempfile()
+
+  download.file(
+    "https://api.dataforsyningen.dk/sogne?format=geojson",
+    temp_file
+  )
+
+  resp_st <- sf::st_read(
+    temp_file,
+    quiet = TRUE
+  )
+
+  map_data <- sf::st_as_sf(resp_st)
+
+  return(map_data)
+}
+
+temp_httr2 <- function() {
+  temp_file <- tempfile()
+
+  temp_file <- dawa(
+    section = "sogne",
+    format = "geojson",
+    verbose = FALSE
+  )
+
+  resp_st <- sf::st_read(
+    temp_file,
+    quiet = TRUE
+  )
+
+  map_data <- sf::st_as_sf(resp_st)
+
+  return(map_data)
+}
+
+manual_passthrough()
+download_file()
+temp_httr2()
+
+
+bench::mark(
+  dawaR::get_map_data("sogne"),
   manual_passthrough(),
+  download_file(),
+  temp_httr2(),
+  iterations = 10,
+  check = FALSE
+)
+
+microbenchmark::microbenchmark(
+  dawaR::get_map_data("sogne"),
+  manual_passthrough(),
+  download_file(),
+  temp_httr2(),
   times = 10
 )
