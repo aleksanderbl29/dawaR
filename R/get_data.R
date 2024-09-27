df8eddcad069c3e861377727cdd83e242703b9fa
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-27 10:00:47 +0200

    Add dynamic dots for get data

--- R/get_data.R
+++ R/get_data.R
@@ -13,7 +13,8 @@
 #' x <- get_data("regioner")
 #' head(x)
 get_data <- function(section,
-                     as_list = FALSE) {
+                     as_list = FALSE,
+                     ...) {
   if (!section %in% available_sections()) {
     cli::cli_abort("You have provided section {.var {section}}.
                    It is not available through the api")
@@ -22,7 +23,8 @@ get_data <- function(section,
   response <- dawa(
     section = section,
     verbose = FALSE,
-    struktur = "flad"
+    struktur = "flad",
+    ... = ...
   )
 
   if (as_list == TRUE) {
