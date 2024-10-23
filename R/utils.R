31003bf9cd0c3c1927f7fd52a1c44957cb6f44ac
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-10-23 17:35:09 +0200

    Add package startup message with https call to the github repo for online updates
    This message should be kept as silent as possible. Preferably never used

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,17 @@
 # onAttach ----
 .onAttach <- function(...) {
-  httr2::request("https://github.com/aleksanderbl29/dawar/")
-
-  packageStartupMessage("## Test")
+  get_status_message() |>
+    packageStartupMessage()
 }
 
-
+# nolint start
+get_status_message <- function() {
+  httr2::request("https://raw.githubusercontent.com/aleksanderbl29/dawaR/refs/heads/main/status-message") |>
+    httr2::req_perform() |>
+    httr2::resp_body_string() |>
+    gsub("\\n", "\n", x = _)
+}
+# nolint end
 
 available_coordinates <- function() {
   x <- c(
